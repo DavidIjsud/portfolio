880aa64e4aadf878d500554d3ddfcb229c3f1564
David Garcia Romero <DavidIjsud@gmail.com>
2025-08-29 18:50:09 -0400

    Update home_about and services_section components to reflect expertise in React Native alongside existing skills in Kotlin and Flutter.

--- src/sections/home/components/home_about.tsx
+++ src/sections/home/components/home_about.tsx
@@ -42,15 +42,15 @@ export default function HomeAbout() {
           padding: { xs: "0 10px", md: 0 },
         }}
       >
-        I'm David, a dedicated Mobile Engineer with over 7 years of experience building high-quality Android and Flutter applications. I specialize in delivering scalable and user-focused solutions, from fintech platforms to e-commerce and logistics apps, with a strong track record at companies like Presto, Loyalty Clubs, and Manzana Verde Inc.
+        I'm David, a dedicated Mobile Engineer with over 7 years of experience building high-quality React Native,Android and Flutter applications. I specialize in delivering scalable and user-focused solutions, from fintech platforms to e-commerce and logistics apps, with a strong track record at companies like Presto, Loyalty Clubs, and Manzana Verde Inc.
         <br />
         <br />
-        My core stack includes Kotlin, Flutter, Firebase, CI/CD pipelines with GitHub Actions, Codemagic and NestJS with TypeScript for backend. I have hands-on experience with secure mobile development practices, modern architecture patterns like MVVM and Clean Architecture, and backend development using NestJS to create robust RESTful and GraphQL APIs.
+        My core stack includes React Native with TypeScript, Kotlin, Flutter, Firebase, CI/CD pipelines with GitHub Actions, Codemagic and NestJS with TypeScript for backend. I have hands-on experience with secure mobile development practices, modern architecture patterns like MVVM and Clean Architecture, and backend development using NestJS to create robust RESTful and GraphQL APIs.
         <br />
         <br />
-        I’m skilled across the full stack, particularly in Flutter, Firebase, and clean architecture, and I pride myself on efficient workflows with
+        I’m skilled across the full stack, particularly in React Native, Kotlin, Flutter, Firebase, and clean architecture, and I pride myself on efficient workflows with
         CI/CD integration using GitHub and Codemagic. No matter the project, I aim to build resilient, scalable code that keeps users coming back.
-        <br/>
+        <br />
         Whether working in startups or enterprise environments, I take full ownership of product development, from planning and implementation to release. I'm passionate about crafting smooth, reliable user experiences and contributing to high-impact mobile products.
       </Box>
       <Link to="Services" smooth={true} duration={1000}>
