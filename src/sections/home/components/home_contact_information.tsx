c3b23b38e8b3576e05ac4b32d6e55847061f27f3
David Garcia Romero <DavidIjsud@gmail.com>
2025-08-29 14:58:52 -0400

    Remove commented-out app deep link options in home contact information component for cleaner code.

--- src/sections/home/components/home_contact_information.tsx
+++ src/sections/home/components/home_contact_information.tsx
@@ -98,12 +98,12 @@ export default function HomeContactInformation() {
       </a>
        */
       }
-      <a href="intent://manzanaverde.la/plans#Intent;scheme=https;package=la.manzana_verde_app;end;">
+      {/* <a href="intent://manzanaverde.la/plans#Intent;scheme=https;package=la.manzana_verde_app;end;">
         Open in app option plans
       </a>
       <a href="intent://manzanaverde.la/?utm_source=franquicia&utm_medium=Miraflores+Arica&utm_campaign=franquicia#Intent;scheme=https;package=la.manzana_verde_app;end;">
         Open with UTMS
-      </a>
+      </a> */}
     </Box>
   );
 }
