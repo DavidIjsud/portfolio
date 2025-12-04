8f16cad4deffcf89d1c8eb3f2d873c9a26e06b83
David Garcia Romero <DavidIjsud@gmail.com>
2025-12-04 16:40:30 -0400

    Refactor portfolio section by removing Abastible project references and simplifying image handling for Drillhub project. Update event handling for project selection and menu interactions.

--- src/sections/home/portfolio_section/fourth_general_row.tsx
+++ src/sections/home/portfolio_section/fourth_general_row.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import { ProjectsProps } from "./first_general_row";
-import { abastibleImages, drillhubImages } from "../../../utils/images";
+import { drillhubImages } from "../../../utils/images";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -8,21 +8,17 @@ import React from "react";
 
 export const FourhtGeneralRow = ({ onOpenDialog }: ProjectsProps) => {
   const [menuAnchor, setMenuAnchor] = React.useState<null | HTMLElement>(null);
-  const [selectedProject, setSelectedProject] = React.useState<null | "abastible" | "drillhub">(null);
 
-  const handleBoxClick = (event: React.MouseEvent<HTMLElement>, project: "abastible" | "drillhub") => {
+  const handleBoxClick = (event: React.MouseEvent<HTMLElement>) => {
     setMenuAnchor(event.currentTarget);
-    setSelectedProject(project);
   };
 
   const handleCloseMenu = () => {
     setMenuAnchor(null);
-    setSelectedProject(null);
   };
 
   const handleViewImages = () => {
-    if (selectedProject === "abastible") onOpenDialog(abastibleImages);
-    else if (selectedProject === "drillhub") onOpenDialog(drillhubImages);
+    onOpenDialog(drillhubImages);
     handleCloseMenu();
   };
 
@@ -37,101 +33,9 @@ export const FourhtGeneralRow = ({ onOpenDialog }: ProjectsProps) => {
         justifyContent: "start",
       }}
     >
-      {/* Abastible Project */}
-      <Box
-        onClick={(e) => handleBoxClick(e, "abastible")}
-        sx={{
-          width: { xs: "100%", md: "33.33%" },
-          height: { xs: 420, md: 800 },
-          backgroundColor: "#E27728",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          flexDirection: "column",
-          position: "relative",
-          mb: { xs: 2, md: 0 },
-          cursor: "pointer",
-          overflow: "hidden",
-          px: { xs: 2, md: 0 },
-          pt: { xs: 2, md: 0 },
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            flexDirection: "row",
-            paddingLeft: { xs: 0, md: "70px" },
-            paddingRight: { xs: 0, md: "70px" },
-            pt: 0,
-            pb: { xs: 2, md: 0 },
-          }}
-        >
-          <h2 style={{ fontSize: 22, margin: 0 }}>Abastible DUO</h2>
-          <ArrowForwardIcon sx={{ cursor: "pointer" }} />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: { xs: "row", md: "column" },
-            width: "100%",
-            height: { xs: 220, md: "100%" },
-            alignItems: "center",
-            justifyContent: "center",
-            gap: { xs: 2, md: 0 },
-            mt: { xs: 2, md: 0 },
-            position: { xs: "static", md: "relative" },
-          }}
-        >
-          <Box
-            component="img"
-            src={abastibleImages[2]}
-            alt="Description of first image"
-            className="imageone"
-            sx={{
-              width: { xs: "60%", md: "50%" },
-              height: { xs: 200, md: "80%" },
-              objectFit: "contain",
-              borderRadius: { xs: 3, md: 0 },
-              boxShadow: { xs: 2, md: 0 },
-              background: { xs: "#fff", md: "none" },
-              p: { xs: 1, md: 0 },
-              position: { xs: "static", md: "absolute" },
-              top: { md: "10%" },
-              left: { md: "10%" },
-              zIndex: 1,
-              margin: { xs: 0, md: 0 },
-              transition: "all 0.5s ease",
-            }}
-          />
-          <Box
-            component="img"
-            src={abastibleImages[4]}
-            className="imagetwo"
-            alt="Description of second image"
-            sx={{
-              width: { xs: "60%", md: "35%" },
-              height: { xs: 200, md: "55%" },
-              objectFit: "contain",
-              borderRadius: { xs: 3, md: 0 },
-              boxShadow: { xs: 2, md: 0 },
-              background: { xs: "#fff", md: "none" },
-              p: { xs: 1, md: 0 },
-              position: { xs: "static", md: "absolute" },
-              top: { md: "40%" },
-              left: { md: "30%" },
-              zIndex: 2,
-              margin: { xs: 0, md: 0 },
-              transition: "all 0.5s ease",
-            }}
-          />
-        </Box>
-      </Box>
       {/* Drillhub Project */}
       <Box
-        onClick={(e) => handleBoxClick(e, "drillhub")}
+        onClick={handleBoxClick}
         sx={{
           width: { xs: "100%", md: "33.33%" },
           height: { xs: 420, md: 800 },
@@ -223,11 +127,6 @@ export const FourhtGeneralRow = ({ onOpenDialog }: ProjectsProps) => {
       </Box>
       <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={handleCloseMenu}>
         <MenuItem onClick={handleViewImages}>View Images</MenuItem>
-        {selectedProject === "abastible" && (
-          <>
-            {/* Add any links for Abastible here if needed */}
-          </>
-        )}
       </Menu>
     </Box>
   );
